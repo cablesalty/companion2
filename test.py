df579ac9c04840c94a00b62632c6b86ffe01ba43
Patrik Szabó <cablesalty@gmail.com>
2024-08-31 18:29:54 +0200

    Remove stuff

--- test.py
+++
@@ -1,8 +0,0 @@
-import pyscreeze
-import pyautogui
-import time
-
-time.sleep(4)
-
-location = pyautogui.locateOnScreen('static/cut/notinlobby_friendcountericon_minifiedsidebar.png')
-print(location)
